7bd3404eabf31a429ac08c6dd1795db239fbe641
nadia-nh <nadia.heredia.nh@gmail.com>
2026-02-21 15:03:07 -0500

    Initialize view model state with a CoinDataSource

--- app/src/main/java/com/example/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
+++ app/src/main/java/com/example/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
@@ -1,11 +1,49 @@
 package com.example.cryptotracker.crypto.presentation.coin_list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cryptotracker.core.domain.util.onError
+import com.example.cryptotracker.core.domain.util.onSuccess
 import com.example.cryptotracker.crypto.domain.CoinDataSource
+import com.example.cryptotracker.crypto.presentation.models.toUICoins
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class CoinListViewModel(): ViewModel() {
+class CoinListViewModel(
+    private val coinDataSource: CoinDataSource
+): ViewModel() {
     private val _state = MutableStateFlow(CoinListState())
-    val state = _state.asStateFlow()
+    val state = _state
+        .onStart { loadCoins() }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000L),
+            CoinListState()
+        )
+
+    private fun loadCoins() {
+        viewModelScope.launch {
+            _state.update {
+                it.copy(isLoading = true)
+            }
+
+            coinDataSource
+                .getCoins()
+                .onSuccess { coins ->
+                    _state.update { it.copy(
+                        isLoading = false,
+                        coins = coins.toUICoins())
+                    }
+                }
+                .onError { error ->
+                    _state.update {
+                        it.copy(isLoading = false)
+                    }
+                }
+        }
+    }
 }
