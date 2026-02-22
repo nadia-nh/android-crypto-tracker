a89004e888a6345a7b6100b4d7cfc80c86b78ee6
nadia-nh <nadia.heredia.nh@gmail.com>
2026-02-21 22:09:15 -0500

    Add a converter from Network Error to a string error message

---
+++ app/src/main/java/com/example/cryptotracker/core/presentation/util/NetworkErrorToString.kt
@@ -0,0 +1,18 @@
+package com.example.cryptotracker.core.presentation.util
+
+import android.content.Context
+import com.example.cryptotracker.R
+import com.example.cryptotracker.core.domain.util.NetworkError
+
+fun NetworkError.toString(context: Context) : String {
+    val resId = when (this) {
+        NetworkError.REQUEST_TIMEOUT -> R.string.error_request_timeout
+        NetworkError.TOO_MANY_REQUESTS -> R.string.error_too_many_requests
+        NetworkError.NO_INTERNET_CONNECTION -> R.string.error_no_internet
+        NetworkError.SERVER_ERROR -> R.string.error_server
+        NetworkError.SERIALIZATION_ERROR -> R.string.error_serialization
+        NetworkError.UNKNOWN_ERROR -> R.string.error_unknown
+    }
+
+    return context.getString(resId)
+}
