c88fd6060274da913e1fa9ba3eaa75d533ff88a7
nadia-nh <nadia.heredia.nh@gmail.com>
2026-02-23 21:51:18 -0500

    Add InfoCard composable

---
+++ app/src/main/java/com/example/cryptotracker/crypto/presentation/coin_detail/components/InfoCard.kt
@@ -0,0 +1,113 @@
+package com.example.cryptotracker.crypto.presentation.coin_detail.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewDynamicColors
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.cryptotracker.R
+import com.example.cryptotracker.ui.theme.CryptoTrackerTheme
+
+@Composable
+fun InfoCard(
+    modifier: Modifier = Modifier,
+    title: String = "",
+    icon: ImageVector = ImageVector.vectorResource(
+        id = R.drawable.dollar
+    ),
+    contentColor: Color = MaterialTheme.colorScheme.onSurface,
+    text: String = "",
+    textStyle: TextStyle = LocalTextStyle.current.copy(
+        textAlign = TextAlign.Center,
+        fontSize = 18.sp,
+        color = contentColor
+    )
+) {
+    Card(
+        modifier = modifier
+            .padding(8.dp)
+            .shadow(
+                elevation = 15.dp,
+                shape = RectangleShape,
+                ambientColor = MaterialTheme.colorScheme.primary,
+                spotColor = MaterialTheme.colorScheme.primary
+            ),
+        shape = RectangleShape,
+        border = BorderStroke(
+            width = 1.dp,
+            color = MaterialTheme.colorScheme.primary
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceContainer,
+            contentColor = contentColor
+        )
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = title,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .size(75.dp)
+                .padding(top = 16.dp),
+            tint = contentColor
+        )
+
+        Spacer(modifier = Modifier.size(8.dp))
+        Text(
+            text = text,
+            style = textStyle,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(horizontal = 16.dp),
+            color = contentColor
+        )
+
+        Spacer(modifier = Modifier.size(8.dp))
+        Text(
+            text = title,
+            style = textStyle,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(horizontal = 16.dp)
+                .padding(bottom = 16.dp),
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Light,
+            textAlign = TextAlign.Center,
+            color = contentColor
+        )
+    }
+}
+
+@Preview
+@PreviewLightDark
+@PreviewDynamicColors
+@Composable
+private fun InfoCardPreview() {
+    CryptoTrackerTheme {
+        InfoCard(
+            title = "Price",
+            text = "$123.456",
+        )
+    }
+}
