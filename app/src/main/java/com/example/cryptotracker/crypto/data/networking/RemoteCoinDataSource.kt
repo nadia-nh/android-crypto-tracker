cc52b152db37c1491f3fb30aa96935e15de3e17b
nadia-nh <nadia.heredia.nh@gmail.com>
2026-02-24 14:38:28 -0500

    Add a getCoinHistory method to the CoinDataSource and implement it

--- app/src/main/java/com/example/cryptotracker/crypto/data/networking/RemoteCoinDataSource.kt
+++ app/src/main/java/com/example/cryptotracker/crypto/data/networking/RemoteCoinDataSource.kt
@@ -6,11 +6,17 @@ import com.example.cryptotracker.core.domain.util.NetworkError
 import com.example.cryptotracker.core.domain.util.Result
 import com.example.cryptotracker.core.domain.util.map
 import com.example.cryptotracker.crypto.data.mappers.toCoin
+import com.example.cryptotracker.crypto.data.mappers.toCoinPrice
+import com.example.cryptotracker.crypto.data.networking.dto.CoinHistoryDto
 import com.example.cryptotracker.crypto.data.networking.dto.CoinResponseDto
 import com.example.cryptotracker.crypto.domain.Coin
 import com.example.cryptotracker.crypto.domain.CoinDataSource
+import com.example.cryptotracker.crypto.domain.CoinPrice
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 class RemoteCoinDataSource(
     private val httpClient: HttpClient
@@ -22,6 +28,32 @@ class RemoteCoinDataSource(
             )
         }.map { response ->
             response.data.map { it.toCoin() }
+        }
+    }
+
+    override suspend fun getCoinHistory(
+        coinId: String,
+        start: ZonedDateTime,
+        end: ZonedDateTime
+    ): Result<List<CoinPrice>, NetworkError> {
+        val startMillis = start
+            .withZoneSameInstant(ZoneId.of("UTC"))
+            .toInstant()
+            .toEpochMilli()
+        val endMillis = end
+            .withZoneSameInstant(ZoneId.of("UTC"))
+            .toInstant()
+            .toEpochMilli()
+        return safeCall<CoinHistoryDto> {
+            httpClient.get(
+                urlString = constructUrl(url = "/assets/$coinId/history")
+            ) {
+                parameter("interval", "h6") // 6 hour interval
+                parameter("start", startMillis)
+                parameter("end", endMillis)
+            }
+        }.map { response ->
+            response.data.map { it.toCoinPrice() }
         }
     }
 }
