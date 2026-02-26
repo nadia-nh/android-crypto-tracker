2a5715b4f2cfad38611c9b40fcf0b89cca17b951
nadia-nh <nadia.heredia.nh@gmail.com>
2026-02-25 22:31:07 -0500

    Add ValueLabel data class
    Used for the line graph drawing in CardDetailScreen

---
+++ app/src/main/java/com/example/cryptotracker/crypto/presentation/coin_detail/ValueLabel.kt
@@ -0,0 +1,24 @@
+package com.example.cryptotracker.crypto.presentation.coin_detail
+
+import java.text.NumberFormat
+import java.util.Locale
+
+data class ValueLabel(
+    val value: Float,
+    val unit: String
+) {
+    fun formatted(): String {
+        val formatter = NumberFormat
+            .getNumberInstance(Locale.getDefault())
+            .apply {
+                val fractionDigits = when {
+                    value > 1000f -> 0
+                    value in 2f..999f -> 2
+                    else -> 3
+                }
+                maximumIntegerDigits = fractionDigits
+                minimumFractionDigits = 0
+            }
+        return formatter.format(value) + unit
+    }
+}
