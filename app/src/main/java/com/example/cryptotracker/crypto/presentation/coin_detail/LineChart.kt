d265752d1eed0341fb5f7ee2119f8907f23970b2
nadia-nh <nadia.heredia.nh@gmail.com>
2026-02-26 15:48:14 -0500

    Draw the background rectangle in LineChart using the Canvas composable

--- app/src/main/java/com/example/cryptotracker/crypto/presentation/coin_detail/LineChart.kt
+++ app/src/main/java/com/example/cryptotracker/crypto/presentation/coin_detail/LineChart.kt
@@ -1,14 +1,17 @@
 package com.example.cryptotracker.crypto.presentation.coin_detail
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.cryptotracker.crypto.domain.CoinPrice
@@ -21,19 +24,65 @@ fun LineChart(
     modifier: Modifier = Modifier,
     dataPoints: List<DataPoint> = emptyList(),
     visibleDataPointsIndices: IntRange= IntRange(0, 0),
+    style: ChartStyle = ChartStyle(),
 ) {
     val visibleDataPoints = remember(dataPoints, visibleDataPointsIndices) {
         dataPoints.slice(visibleDataPointsIndices)
     }
+    val measurer = rememberTextMeasurer()
 
-    Column(
+    val textStyle = TextStyle.Default.copy(fontSize = style.labelFontSize)
+
+    Canvas(
         modifier = modifier
     ) {
-        visibleDataPoints.forEachIndexed { index, point ->
-            Text(
-                text = "$index - ${point.xLabel} x: ${point.x}, y: ${point.y}"
+        val xLabelTextLayoutResults = visibleDataPoints.map {
+            measurer.measure(
+                text = it.xLabel,
+                style = textStyle.copy(textAlign = TextAlign.Center)
             )
         }
+
+        // X-coordinate
+        val horizontalPaddingPx = style.horizontalPadding.roundToPx()
+        val xAxisLabelSpacingPx = style.xAxisLabelSpacing.roundToPx()
+
+        val maxXLabelWidth = xLabelTextLayoutResults
+            .maxOfOrNull { it.size.width } ?: 0
+        val viewportRightX = size.width
+        val viewportLeftX = 2 * horizontalPaddingPx.toFloat()
+
+        // Y-coordinate
+        val verticalPaddingPx = style.verticalPadding.roundToPx()
+
+        val maxXLabelHeight = xLabelTextLayoutResults
+            .maxOfOrNull { it.size.height } ?: 0
+        val maxXLabelLineCount = xLabelTextLayoutResults
+            .maxOfOrNull { it.lineCount } ?: 0
+
+        val xLabelLineHeight = if (maxXLabelLineCount > 0) {
+            maxXLabelHeight / maxXLabelLineCount
+        } else {
+            0
+        }
+
+        val viewportHeightPx = size.height - (
+                maxXLabelHeight + 2 * verticalPaddingPx + xLabelLineHeight + xAxisLabelSpacingPx)
+        val viewportTopY = verticalPaddingPx + xLabelLineHeight + 10f
+        val viewportBottomY = viewportTopY + viewportHeightPx
+
+        val viewport = Rect(
+            top = viewportTopY,
+            left = viewportLeftX,
+            bottom = viewportBottomY,
+            right = viewportRightX
+        )
+
+        drawRect(
+            color = Color.Green,
+            topLeft = viewport.topLeft,
+            size = viewport.size,
+        )
     }
 }
 
